fc6ec1b35f3b99e01c8a4184db95bac2ddd23f22
sauvio <sauvio.liu@gmail.com>
2021-06-15 11:09:10 +0800

    add tips

--- build-image.sh
+++ build-image.sh
@@ -5,9 +5,14 @@ ENTRYPOINT ["java","-jar","-Xms64m","-Xmx128m","/springboot-demo.jar"]
 EOF
 
 echo "Dockerfile created successfully!"
+app="springboot-demo:1.0"
 
 # 基于指定目录下的Dockerfile构建镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/sauvio/springboot-demo:1.0 .
+docker build -t registry.cn-hangzhou.aliyuncs.com/sauvio/${app} .
+
+echo "docker build ${app} successfully!"
 
 # push镜像，这边需要阿里云镜像仓库登录，在w2上登录
-docker push registry.cn-hangzhou.aliyuncs.com/sauvio/springboot-demo:1.0
+docker push registry.cn-hangzhou.aliyuncs.com/sauvio/${app}
+
+echo "docker push ${app} successfully!"
